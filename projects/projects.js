47e591303dcfbbf1b803a444c7557486305b6831
mparkms <mikeparkms@gmail.com>
2015-10-23 22:20:45 -0700

    Titles and stuff

--- projects/projects.js
+++ projects/projects.js
@@ -1,10 +1,13 @@
 $(function() {
 	$('.show-hide-btn').click(function() {
 		var details = $(this).parent().parent().nextAll('.project-details:first');
-		if (details.css('display') == 'none')
+		if (details.css('display') == 'none') {
 			details.show(300);
-		else
+			$(this).text("Hide details");
+		} else {
 			details.hide(300);
+			$(this).text("Show details");
+		}
 	});
 })
 
